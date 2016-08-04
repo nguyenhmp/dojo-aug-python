f6b3db5327bdd2ab2709d3ebcddf2f0505feccf7
snebaybay <spokharel1991@gmail.com>
2016-08-04 14:59:34 -0400

    Landing Assignment

--- SnehaPokharel/underscore.py
+++ SnehaPokharel/underscore.py
@@ -30,15 +30,17 @@ class Underscore(object):
         
 # you just created a library with 5 methods!
 # let's create an instance of our class
-underscore1 = Underscore() # yes we are setting our instance to a variable that is an underscore
+underscore1 = Underscore()
+reducing = underscore1.reduce([1, 2, 3, 4, 5, 6], (lambda x, y: x + y), 0)
+return reducing 
+ # yes we are setting our instance to a variable that is an underscore
 # evens = underscore1.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # should return [2, 4, 6] after you finish implementing the code above
 # mappings = underscore1.map([1, 2, 3, 4, 5, 6], lambda x: x * 3)
 # print mappings
-# reducing = underscore1.reduce([1, 2, 3, 4, 5, 6], (lambda x, y: x + y), 0)
 # print reducing 
 # finds = underscore1.find([1, 2, 3, 3, 3, 3], lambda x: x % 2 == 0)
 # print finds
-rejecting = underscore1.reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
-print rejecting
+# rejecting = underscore1.reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+# print rejecting
 
